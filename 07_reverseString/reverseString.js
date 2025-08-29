3c3146256642e31e08e91dc1d0148fddea08765e
Henry goodluck <chigoziehenry852@gmail.com>
2025-08-29 16:52:37 +0100

    complete 07_reaverseString exercise

--- 07_reverseString/reverseString.js
+++ 07_reverseString/reverseString.js
@@ -1,6 +1,10 @@
-const reverseString = function() {
-
+const reverseString = function(str) {
+  let chars = str.split("");
+  let reversed = chars.reverse();
+  console.log('About to return:', reversed.join("")); // just log the value
+  return reversed.join(""); // return separately
 };
 
 // Do not edit below this line
 module.exports = reverseString;
+	
