db18ef90ffe10f31fae56faaea2b817fba6257d7
Henry goodluck <chigoziehenry852@gmail.com>
2025-08-30 15:49:29 +0100

    complete 08_removeFromeArray exercise

--- 08_removeFromArray/removeFromArray.js
+++ 08_removeFromArray/removeFromArray.js
@@ -1,4 +1,6 @@
-const removeFromArray = function() {
+const removeFromArray = function(arr,...args) {
+  // args collects all the extra values after arr 
+  return arr.filter(item => !args.includes(item));
 };
 
 // Do not edit below this line
