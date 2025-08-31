81bd002638a9c667e3ebbc2114f1aef6f52964c2
Henry goodluck <chigoziehenry852@gmail.com>
2025-08-31 17:49:21 +0100

    complete 09_sumAll exercise

--- 09_sumAll/sumAll.js
+++ 09_sumAll/sumAll.js
@@ -1,6 +1,17 @@
-const sumAll = function() {
+const sumAll = function(a, b) {
+	//cheak for negative numbers, or non-numbers. 
+	if (a < 0 || b < 0) return "ERROR"; 
+	if (typeof a !== "number" || typeof b !== "number") return "ERROR"; 
+	if (!Number.isInteger(a) || !Number.isInteger(b)) return "ERROR";
+const start = Math.min(a, b);
+const end = Math.max(a, b);
 
+let total = 0;
+for(let i = start; i <= end; i++)  {
+	total += i;
+}
+return total; 
 };
-
+console.log(sumAll(2, 4));
 // Do not edit below this line
 module.exports = sumAll;
