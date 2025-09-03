130026976314cb8f134a7672ae1a348c7161b1e8
Henry goodluck <chigoziehenry852@gmail.com>
2025-09-03 12:24:55 +0100

    complete 12_calculator exercise

--- 12_calculator/calculator.js
+++ 12_calculator/calculator.js
@@ -1,25 +1,40 @@
-const add = function() {
-	
+const add = function(a, b) {
+return a + b; 
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b) {
+	return a - b
 };
 
-const sum = function() {
-	
+// sum all numbers in an array
+const sum = function(numbers) {
+	let total = 0;
+	for(let num of numbers) {
+		total += num;
+	}
+	return total; 
 };
 
-const multiply = function() {
-
+const multiply = function(numbers) {
+	let result = 1; // start with 1, so multiplication works correctly
+	for (let num of numbers) {
+		result *= num
+	}
+return result
 };
 
-const power = function() {
-	
+// Raise a number to the power 
+const power = function(base, exponent) {
+	return base ** exponent; // or Math.power(base, exponent) 
 };
 
-const factorial = function() {
-	
+const factorial = function(n) {
+	if (n === 0) return 1; // special case 0! = 1
+	let result = 1; // start with 1 (because multiplying by 1 changes nothing)
+	for (let i = 1; i <= n; i++) {
+		result *= i
+	} 
+	return result
 };
 
 // Do not edit below this line
