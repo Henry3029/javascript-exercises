dfa5fab4e971c67aaba1cca4f7d530eb7f508bc4
Henry goodluck <chigoziehenry852@gmail.com>
2025-09-03 16:40:32 +0100

    complete 13_palimdromes exercise

--- 13_palindromes/palindromes.js
+++ 13_palindromes/palindromes.js
@@ -1,5 +1,10 @@
-const palindromes = function () {
-
+const palindromes = function (str) {
+// make everything lowercase and remove punctuation and space 
+const cleaned = str.toLowerCase().replace(/[^a-z0-9]/gi, "");
+// reverse the cleaned string 
+const reversed = cleaned.split("").reverse().join("");
+// compere forward vs reversed
+return cleaned === reversed; 
 };
 
 // Do not edit below this line
