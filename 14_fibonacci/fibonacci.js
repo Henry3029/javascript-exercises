2b3c378e34fb60d7dc359e8ce2cad933a7f213a9
Henry goodluck <chigoziehenry852@gmail.com>
2025-09-04 10:27:49 +0100

    complete 14_fibonacci exercise

--- 14_fibonacci/fibonacci.js
+++ 14_fibonacci/fibonacci.js
@@ -1,5 +1,21 @@
-const fibonacci = function() {
+const fibonacci = function(n) {
+	// convert strings to numbers
+	n = Number(n);
+// handle invalid input
+if (n <= 0 || !Number.isInteger(n)) return "OOPS";
 
+// the first two numbers are always 1
+if (n === 1 || n === 2) return 1;
+
+let first = 1;
+let second = 1;
+let next = 0;
+for (let i = 3; i <= n; i++) {
+	next = first + second; // sum of two previous 
+	first = second; // move forward 
+	second = next; 
+}
+return second;
 };
 
 // Do not edit below this line
