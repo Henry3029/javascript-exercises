f2bec165001b4dd5941138dc07b5e274f6e60aee
Henry goodluck <chigoziehenry852@gmail.com>
2025-09-05 09:37:26 +0100

    complete 16_findTheOldest exercise

--- 16_findTheOldest/findTheOldest.js
+++ 16_findTheOldest/findTheOldest.js
@@ -1,5 +1,16 @@
-const findTheOldest = function() {
+const findTheOldest = function(people) {
+  // helper function to calculate age
+  const getAge = function(person) {
+    const deathYear = person.yearOfDeath || new Date().getFullYear();
+    return deathYear - person.yearOfBirth;
+  };
 
+  // use reduce to find the oldest person 
+  return people.reduce((oldest, currentPerson) => {
+    const oldestAge = getAge(oldest);
+    const currentAge = getAge(currentPerson);
+    return currentAge > oldestAge ? currentPerson : oldest;
+  });
 };
 
 // Do not edit below this line
