e293337b7fc4f00aa0736e5b036e77266b018dbb
Henry goodluck <chigoziehenry852@gmail.com>
2025-09-08 19:11:54 +0100

    complete archived_snakeCase exercise

--- archive/archived_snakeCase/snakeCase.js
+++ archive/archived_snakeCase/snakeCase.js
@@ -1,5 +1,15 @@
-const snakeCase = function() {
-
+const snakeCase = function(str) {
+return str
+// insert space between lowercase and uppercase (camelCase -> camel case)
+.replace(/([a-z])([A-Z])/g, '$1 $2')
+// replace all non-numbers/laters with space
+.replace(/[^a-zA-Z0-9]/g, ' ')
+// lowercase everything 
+.toLowerCase()
+// remove leading/trailing spaces
+.trim()
+// replace spaces with underscores
+.replace(/\s+/g, '_');
 };
 
 // Do not edit below this line
